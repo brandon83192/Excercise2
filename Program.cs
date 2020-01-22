f4b239e05d5059d1d9dc3a8f4a08a3f97c6c1b32
Btond <Btond@LAPTOP-K8U30UIB>
2020-01-21 20:59:08 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Excercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            int height;
+            age = 27;
+            height = 72;
+            int maxheartrate = 220 - age;
+
+            Console.WriteLine("I am " + age + "years old, my max heart rate is " + maxheartrate + " and I am " + height + " inches tall");
+         
+        }
+    }
+}
